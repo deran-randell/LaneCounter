08e065b5c03f943192e2519ad19acada00c45e70
Denver <denver@cybicom.com>
2022-04-22 13:55:21 +0200

    Fixed: CORS Policy Error on Chrome Browser

--- web_app/server/app.js
+++ web_app/server/app.js
@@ -22,18 +22,18 @@ app.set('view engine', 'jade');
 
 const whitelist = ["http://localhost:3000", "http://localhost:3001"]
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
+  origin:true,
+  // origin: function (origin, callback) {
+  //   if (!origin || whitelist.indexOf(origin) !== -1) {
+  //     callback(null, true)
+  //   } else {
+  //     callback(new Error("Not allowed by CORS"))
+  //   }
+  // },
+   credentials: true,
 }
 
 app.use(cors(corsOptions))
-
 
 app.use(logger('dev'));
 app.use(express.json());
